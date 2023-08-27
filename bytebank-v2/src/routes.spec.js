1a49121ba4fb65fd6f11e228e94c91683714c306
bbeluco <beluco.bispo@hotmail.com>
2023-08-27 16:38:21 -0300

    Usando memoryRouter para testar rotas

--- bytebank-v2/src/routes.spec.js
+++ bytebank-v2/src/routes.spec.js
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './paginas/Principal/App';
+import Cartoes from './componentes/Cartoes/index';
 
 describe('Rotas', () => {
   test('Rota principal', () => {
     render(<App />, { wrapper: BrowserRouter });
     const rotaPrincipal = screen.getByText('Olá, Joana :)!');
     expect(rotaPrincipal).toBeInTheDocument();
+  });
+
+  test('Rota de cartoes', () => {
+    const rota = '/cartoes';
+
+    render(
+      <MemoryRouter initialEntries={[rota]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="cartoes" element={<Cartoes />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const tituloPaginaCartoes = screen.getByTestId('titulo-principal');
+    expect(tituloPaginaCartoes).toHaveTextContent('Meus cartões');
   });
 });
