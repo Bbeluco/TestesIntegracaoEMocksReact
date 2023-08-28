a0a989a472c7c190d849332fa5abb27f95e964e9
bbeluco <beluco.bispo@hotmail.com>
2023-08-28 19:46:10 -0300

    Testando hook personalizado e assincrono

--- bytebank-v2/src/hooks/hooks.spec.js
+++ bytebank-v2/src/hooks/hooks.spec.js
@@ -1,5 +1,19 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useEffect, useState } from 'react';
+import { buscaTransacoes } from '../services/transacoes';
+import useListaTransacoes from './useListaTransacoes';
+
+jest.mock('../services/transacoes');
+
+const mockTransacao = [
+  {
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '22/11/2022',
+    mes: 'Novembro',
+  },
+];
 
 describe('Testando hooks', () => {
   test('Testando hoook nativo react', () => {
@@ -14,5 +28,17 @@ describe('Testando hooks', () => {
     });
 
     expect(result.current).toBe('Bruno');
+  });
+
+  test('Testando hook personalizado', async () => {
+    buscaTransacoes.mockImplementation(() => mockTransacao);
+
+    const { result } = renderHook(() => useListaTransacoes());
+
+    await act(async () => {
+      result.current[1]();
+    });
+
+    expect(result.current[0]).toEqual(mockTransacao);
   });
 });
